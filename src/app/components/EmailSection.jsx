3b0c2a03f682a62b0f7a599601dbccf01fcab889
roannarum <dewiarumsari38@gmail.com>
2024-09-24 21:48:51 +0700

    apos fix

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -14,11 +14,14 @@ const EmailSection = () => {
     >
       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
       <div className="z-10">
-        <h5 className="text-xl font-bold text-white my-2">Let&apos;s Connect</h5>
+        <h5 className="text-xl font-bold text-white my-2">
+          Let&apos;s Connect
+        </h5>
         <p className="text-[#ADB7BE] mb-4 max-w-md">
-          Let's Connect
-I’m always open to exciting opportunities and collaborations. If you have any questions, want to discuss a project, or just want to chat, feel free to reach out. I’ll do my best to respond promptly!
-
+          Let's Connect I&apos;m always open to exciting opportunities and
+          collaborations. If you have any questions, want to discuss a project,
+          or just want to chat, feel free to reach out. I&apos;ll do my best to
+          respond promptly!
         </p>
         <div className="socials flex flex-row gap-4 justify-center">
           <Link href="https://github.com/roannarum">
